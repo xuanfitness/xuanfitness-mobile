bb630f3788684b336e471461cdd7cb8fc55a6e2b
Emilywang98 <ewang56@uwo.ca>
2021-01-28 21:29:26 -0700

    i cleaned up stuff so it can run.

---
+++ xuan_fitness/lib/pages/loginhome/home_calendar.dart
@@ -0,0 +1,9 @@
+/*import 'package:flutter/semantics.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter/material.dart';
+import 'package:xuan_fitness/widgets/logo_widget.dart';
+
+class _HomeCalendar extends StatefulWidget {}
+
+class _State extends State<_HomeCalendar> 
+}*/
