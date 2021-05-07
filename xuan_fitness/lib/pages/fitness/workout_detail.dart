39ccd0be02921e68a3529b3cc081e47fdada4f0a
Vivian Wu <wu.vivian00@gmail.com>
2021-05-07 14:48:19 -0700

    Fix nested scrollview rendering bug

--- xuan_fitness/lib/pages/fitness/workout_detail.dart
+++ xuan_fitness/lib/pages/fitness/workout_detail.dart
@@ -26,81 +26,167 @@ class _WorkoutDetailState extends State<WorkoutDetail> {
   @override
   static String formattedDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
   Widget build(BuildContext context) {
-    return new Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            //backgroundColor: Color(0xFFFFEB3),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              Container(
-                margin: EdgeInsets.all(8.0),
+    return LayoutBuilder(builder: (BuildContext context, BoxConstraints viewportConstraints){
+      return Material(
+        child: SingleChildScrollView(
+            child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: viewportConstraints.maxHeight),
+            child: Container(
+              padding: EdgeInsets.fromLTRB(5, 25, 5, 0),
+              color: Colors.white,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(8.0),
 
-                decoration: BoxDecoration(
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.white,
-                      blurRadius: 1,
-                      //offset: Offset(0, 2),
-                    ),
-                  ],
-                ),
-                //color: Colors.green,
-                padding: const EdgeInsets.only(bottom: 8),
-                //child: Scrollbar(isAlwaysShown:true, controller:_scrollController,)
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Workouts',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontFamily: 'cabin',
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF6A8D73),
-
-                            //fontWeight: w600
+                      decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                            color: Colors.white,
+                            blurRadius: 1,
+                            //offset: Offset(0, 2),
                           ),
-                        ),
+                        ],
                       ),
-                      SizedBox(height: 30),
-                      Text('Today',
-                          style: TextStyle(
-                            fontFamily: 'cabin',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFFA2C2A9),
-                          )),
-                      Text('$formattedDate',
-                          style: TextStyle(
-                            fontFamily: 'cabin',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF6A8D73),
-                          )),
-                    ]),
-              ),
-              new ListView.builder(
-                padding: const EdgeInsets.all(20),
-                itemCount: entries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    elevation: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.all(100),
-                      child: ListTile(
-                        title: Text('${entries[index]}'),
-                        subtitle: Text('${subentries[index]}'),
-                      ),
+                      //color: Colors.green,
+                      padding: const EdgeInsets.only(bottom: 8),
+                      //child: Scrollbar(isAlwaysShown:true, controller:_scrollController,)
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Workouts',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontFamily: 'cabin',
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF6A8D73),
+
+                                  //fontWeight: w600
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            Text('Today',
+                                style: TextStyle(
+                                  fontFamily: 'cabin',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFA2C2A9),
+                                )),
+                            Text('$formattedDate',
+                                style: TextStyle(
+                                  fontFamily: 'cabin',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF6A8D73),
+                                )),
+                          ]),
                     ),
-                    color: Colors.green[colorCodes[index]],
-                  );
-                },
-              ),
-            ])));
+                    ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.all(20),
+                        itemCount: entries.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            elevation: 5,
+                            child: Padding(
+                              padding: const EdgeInsets.all(100),
+                              child: ListTile(
+                                title: Text('${entries[index]}'),
+                                subtitle: Text('${subentries[index]}'),
+                              ),
+                            ),
+                            color: Colors.green[colorCodes[index]],
+                          );
+                        },
+                      ),
+                  ]),
+            ),
+        )),
+      );
+    });
+            // backgroundColor: Color(0xFFFFEB3),
+            // child: Container(
+            //   color: Colors.white,
+            //   child: Column(
+            //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //       children: [
+            //         Text("hello"),
+            //     Container(
+            //       margin: EdgeInsets.all(8.0),
+            //
+            //       decoration: BoxDecoration(
+            //         boxShadow: <BoxShadow>[
+            //           BoxShadow(
+            //             color: Colors.white,
+            //             blurRadius: 1,
+            //             //offset: Offset(0, 2),
+            //           ),
+            //         ],
+            //       ),
+            //       //color: Colors.green,
+            //       padding: const EdgeInsets.only(bottom: 8),
+            //       //child: Scrollbar(isAlwaysShown:true, controller:_scrollController,)
+            //       child: Column(
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: <Widget>[
+            //             Align(
+            //               alignment: Alignment.centerLeft,
+            //               child: Text(
+            //                 'Workouts',
+            //                 textAlign: TextAlign.left,
+            //                 style: TextStyle(
+            //                   fontFamily: 'cabin',
+            //                   fontSize: 40,
+            //                   fontWeight: FontWeight.bold,
+            //                   color: Color(0xFF6A8D73),
+            //
+            //                   //fontWeight: w600
+            //                 ),
+            //               ),
+            //             ),
+            //             SizedBox(height: 30),
+            //             Text('Today',
+            //                 style: TextStyle(
+            //                   fontFamily: 'cabin',
+            //                   fontSize: 20,
+            //                   fontWeight: FontWeight.bold,
+            //                   color: Color(0xFFA2C2A9),
+            //                 )),
+            //             Text('$formattedDate',
+            //                 style: TextStyle(
+            //                   fontFamily: 'cabin',
+            //                   fontSize: 20,
+            //                   fontWeight: FontWeight.bold,
+            //                   color: Color(0xFF6A8D73),
+            //                 )),
+            //           ]),
+            //     ),
+            //     new ListView.builder(
+            //       padding: const EdgeInsets.all(20),
+            //       itemCount: entries.length,
+            //       itemBuilder: (BuildContext context, int index) {
+            //         return Card(
+            //           elevation: 5,
+            //           child: Padding(
+            //             padding: const EdgeInsets.all(100),
+            //             child: ListTile(
+            //               title: Text('${entries[index]}'),
+            //               subtitle: Text('${subentries[index]}'),
+            //             ),
+            //           ),
+            //           color: Colors.green[colorCodes[index]],
+            //         );
+            //       },
+            //     ),
+            //   ]),
+            // ));
   }
 
   Widget cryptoNameSymbol() {
