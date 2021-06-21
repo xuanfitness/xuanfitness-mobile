1c0b7fd2c0b48f40abdbf00804148c68a294a1a0
Emily Wang <ewang56@uwo.ca>
2021-06-21 15:23:38 -0700

    Fix widget render errors

--- xuan_fitness/lib/main.dart
+++ xuan_fitness/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:xuan_fitness/pages/auth/login.dart';
 import 'package:xuan_fitness/repositories/user_repository.dart';
 import 'package:xuan_fitness/navigation/task_bar.dart';
 
-void main(){
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
@@ -15,48 +15,47 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          theme: ThemeData(
+        theme: ThemeData(
             primaryColor: Color(0xFF6A8D73),
             primaryColorDark: Color(0xFF475E4D),
             accentColor: Color(0xffCFE8D5),
-            primaryColorLight: Colors.white
-          ),
-          home: FutureBuilder(
-            future: _initialization,
-            builder: (context, snapshot){
-              if (snapshot.hasError) {
-                print(snapshot.error);
-                return null;
-              }
-              if (snapshot.connectionState == ConnectionState.done) {
-                return HomePage();
-              }
-              return Splash();
-            },
-          )
-    );
+            primaryColorLight: Colors.white),
+        home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return SizedBox(child: Text(snapshot.error.toString()));
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return HomePage();
+            }
+            return Splash();
+          },
+        ));
   }
 }
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-        create: (_) => UserRepository.instance(),
-        child: Consumer(
-          builder: (context, UserRepository user, _){
-            switch(user.status){
-              case Status.Uninitialized:
-                return Splash();
-              case Status.Unauthenticated:
-              case Status.Authenticating:
-                return LoginPage();
-              case Status.Authenticated:
-                return TaskBar();
-              default:
-                return null;
-            }
-          },
-        ),
+      create: (_) => UserRepository.instance(),
+      child: Consumer(
+        builder: (context, UserRepository user, _) {
+          switch (user.status) {
+            case Status.Uninitialized:
+              return Splash();
+            case Status.Unauthenticated:
+            case Status.Authenticating:
+              return LoginPage();
+            case Status.Authenticated:
+              return TaskBar();
+            default:
+              return null;
+          }
+        },
+      ),
     );
   }
 }
